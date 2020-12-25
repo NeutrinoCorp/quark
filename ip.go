7cc647477792f4764209e13db712218928d327ee
A Ruiz <luis.alonso.16@hotmail.com>
2020-12-24 19:13:38 -0700

    Improve devastating malloc to get a local ip, still marked function as deprecated

--- ip.go
+++ ip.go
@@ -1,20 +1,17 @@
 package quark
 
-import "net"
+import (
+	"net"
+)
 
-// getLocalIP returns the non loopback local IP of the host
-func getLocalIP() string {
-	addrs, err := net.InterfaceAddrs()
+// 	deprecated
+// GetLocalIP returns the non loopback local IP of the host
+func GetLocalIP() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	// handle err...
 	if err != nil {
 		return ""
 	}
-	for _, address := range addrs {
-		// check the address type and if it is not a loopback the display it
-		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				return ipnet.IP.String()
-			}
-		}
-	}
-	return ""
+	defer conn.Close()
+	return conn.LocalAddr().(*net.UDPAddr).IP.String()
 }
