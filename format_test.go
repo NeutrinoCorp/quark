2f40e63caae95c5e9cc3fae601ec68aef428bde4
A Ruiz <luis.alonso.16@hotmail.com>
2021-01-11 20:11:06 -0700

    Fix misspells

--- format_test.go
+++ format_test.go
@@ -8,8 +8,8 @@ import (
 
 func TestFormatTopicName(t *testing.T) {
 	t.Run("Comply with Topic Async API specification", func(t *testing.T) {
-		correctName := "neutrino.payment.1.domain_event.user.payed_out"
-		topic := FormatTopicName("neutrino", "payment", DomainEvent, "user", "payed_out", 1)
+		correctName := "neutrino.payment.1.domain_event.user.paid"
+		topic := FormatTopicName("neutrino", "payment", DomainEvent, "user", "paid", 1)
 		assert.Equal(t, correctName, topic)
 	})
 }
@@ -18,15 +18,15 @@ func BenchmarkFormatTopicName(b *testing.B) {
 	b.Run("Form a valid Async API topic name", func(b *testing.B) {
 		b.ReportAllocs()
 		for i := 0; i < b.N; i++ {
-			_ = FormatTopicName("neutrino", "payment", DomainEvent, "user", "payed_out", 1)
+			_ = FormatTopicName("neutrino", "payment", DomainEvent, "user", "paid", 1)
 		}
 	})
 }
 
 func TestFormatQueueName(t *testing.T) {
 	t.Run("Comply with Queue Async API specification", func(t *testing.T) {
-		correctName := "payment.organization.increment_sales_on_user_payed_out"
-		topic := FormatQueueName("payment", "organization", "increment_sales", "user_payed_out")
+		correctName := "payment.organization.increment_sales_on_user_paid"
+		topic := FormatQueueName("payment", "organization", "increment_sales", "user_paid")
 		assert.Equal(t, correctName, topic)
 	})
 }
@@ -35,7 +35,7 @@ func BenchmarkFormatQueueName(b *testing.B) {
 	b.Run("Form a valid Async API queue name", func(b *testing.B) {
 		b.ReportAllocs()
 		for i := 0; i < b.N; i++ {
-			_ = FormatQueueName("payment", "organization", "increment_sales", "user_payed_out")
+			_ = FormatQueueName("payment", "organization", "increment_sales", "user_paid")
 		}
 	})
 }
