94202e80209ced0aef9c47329a770e81a16ed621
A Ruiz <luis.alonso.16@hotmail.com>
2021-04-20 18:11:20 -0600

    Add more accurate simple example, Use cluster for simple example

--- bus/kafka/examples/simple-consumers/main.go
+++ bus/kafka/examples/simple-consumers/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"errors"
 	"log"
 	"os"
 	"os/signal"
@@ -21,32 +20,44 @@ func main() {
 	// Create broker
 	b := kafka.NewKafkaBroker(
 		newSaramaCfg(),
-		quark.WithCluster("localhost:9092"),
+		quark.WithCluster("localhost:19092", "localhost:29092", "localhost:39092"),
 		quark.WithBaseMessageSource("https://neutrinocorp.org/cloudevents"),
 		quark.WithBaseMessageContentType("message/partial"),
 		quark.WithErrorHandler(customErrHandler))
 
-	b.Topics("chat.0").PoolSize(1).MaxRetries(3).
+	b.Topics("org.neutrinocorp.command.user.create").Group("user-service").PoolSize(5).MaxRetries(3).
 		HandleFunc(func(w quark.EventWriter, e *quark.Event) bool {
 			log.Printf("topic: %s | message: %s", e.Topic, e.RawValue)
 			log.Printf("topic: %s | redelivery: %s", e.Topic, e.Header.Get(quark.HeaderMessageRedeliveryCount))
-			_, _ = w.Write(e.Context, []byte(e.Body.Data), "chat.1")
+			_, _ = w.Write(e.Context, []byte(e.Body.Data), "org.neutrinocorp.event.user.created")
 			return true
 		})
 
-	b.Topics("chat.1").Group("foo-group").PoolSize(2).MaxRetries(3).
+	b.Topics("org.neutrinocorp.event.user.created").Group("notification-service").PoolSize(5).MaxRetries(3).
 		HandleFunc(func(w quark.EventWriter, e *quark.Event) bool {
 			redelivery := e.Header.Get(quark.HeaderMessageRedeliveryCount)
 			log.Printf("topic: %s | message: %s", e.Topic, e.RawValue)
 			log.Printf("topic: %s | redelivery: %s", e.Topic, redelivery)
-			err := w.WriteRetry(e.Context, e.Body)
-			if errors.Is(err, quark.ErrMessageRedeliveredTooMuch) {
-				_, _ = w.Write(e.Context, e.Body.Data, "dlq.chat.1")
-			}
+
+			/*
+				// Retry strategy
+				err := w.WriteRetry(e.Context, e.Body)
+				if errors.Is(err, quark.ErrMessageRedeliveredTooMuch) {
+					_, _ = w.Write(e.Context, e.Body.Data, "dlq.chat.1")
+				}*/
 			return true
 		})
 
-	b.Topic("dlq.chat.1").Group("foo-group").
+	b.Topics("org.neutrinocorp.event.user.created").Group("analytics-service").PoolSize(5).MaxRetries(3).
+		HandleFunc(func(w quark.EventWriter, e *quark.Event) bool {
+			redelivery := e.Header.Get(quark.HeaderMessageRedeliveryCount)
+			log.Printf("topic: %s | message: %s", e.Topic, e.RawValue)
+			log.Printf("topic: %s | redelivery: %s", e.Topic, redelivery)
+			_, _ = w.Write(e.Context, e.Body.Data, "org.neutrinocorp.event.user.created.dlq")
+			return true
+		})
+
+	b.Topic("org.neutrinocorp.event.user.created.dlq").Group("foo-group").
 		HandleFunc(func(w quark.EventWriter, e *quark.Event) bool {
 			log.Printf("topic: %s | message: %s", e.Topic, string(e.Body.Data))
 			return true
@@ -77,8 +88,10 @@ func main() {
 func newSaramaCfg() *sarama.Config {
 	config := sarama.NewConfig()
 	config.ClientID = "neutrino-sample"
+	config.Consumer.Offsets.Initial = sarama.OffsetOldest
 	config.Consumer.Return.Errors = true
 	config.Producer.Return.Successes = true
 	config.Producer.Return.Errors = true
+	config.Consumer.Retry.Backoff = time.Second * 10
 	return config
 }
